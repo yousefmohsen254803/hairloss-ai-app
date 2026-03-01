39390cda38350568a26e9b1fa2b46ede404d7e84
yousefmohsen254803 <254803@buas.nl>
2026-03-02 00:38:40 +0100

    size udpate

--- Home.py
+++ Home.py
@@ -83,7 +83,7 @@ st.markdown(
 # Hero image smaller + centered
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image("assets/example.jpg", width=500)
+    st.image("assets/example.jpg", width=700)
 
 st.markdown("<br>", unsafe_allow_html=True)
 
