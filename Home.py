9d2980abb868a8b1b3d4b5d125ce9027a17314a4
yousefmohsen254803 <254803@buas.nl>
2026-03-02 00:45:03 +0100

    size

--- Home.py
+++ Home.py
@@ -81,7 +81,7 @@ st.markdown(
 
 
 # Example image smaller + centered
-col1, col2, col3 = st.columns([1, 2, 1])
+col1, col2, col3 = st.columns([1, 4, 1])
 with col2:
     st.image("assets/example.jpg", width=2000)
 
