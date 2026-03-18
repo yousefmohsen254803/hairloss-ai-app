2b741c71b5f5ac088ff7b6855eddd50abec2d020
yousefmohsen254803 <254803@buas.nl>
2026-03-18 06:42:29 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -188,7 +188,7 @@ st.markdown("<br>", unsafe_allow_html=True)
 # -----------------------------
 # Button
 # -----------------------------
-col1, col2, col3 = st.columns([1, 2, 1])
+col1, col2, col3 = st.columns([1, 1, 1])
 
 with col2:
     if st.button("🔍 Analyse My Hair Loss"):
