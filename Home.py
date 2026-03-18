049c80dcf5fc2726fdad9de2d1b327b69deb9b76
yousefmohsen254803 <254803@buas.nl>
2026-03-18 06:38:55 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -54,7 +54,7 @@ def set_background_png(path: str):
         div.stButton > button {{
             border-radius:14px;
             padding:12px 16px;
-            font-weight:900;
+            font-weight:700;
         }}
 
         .example-img img {{
